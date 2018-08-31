13d6893d70960330865d2f22fd934a52a7032c28
NatashaSenah <tashsenah@gmail.com>
2018-08-31 14:55:42 +0300

    add the created news

--- app/config.py
+++ app/config.py
@@ -1,27 +1,22 @@
-class Config:
-    '''
-    General configuration parent class
-    '''
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/{}?api_key={}'
+import os
 
+class Config:
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
+    
     DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MOVIE_API_KEY='11ec9236afeb7dc47355465af1b10af5'
+export SECRET_KEY='Flask WTF Secret key'
+python3.6 manage.py server
