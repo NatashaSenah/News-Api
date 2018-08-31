c20bc67590a1084c908733f4d4ddaebf062caccb
NatashaSenah <tashsenah@gmail.com>
2018-08-31 11:42:20 +0300

    add the created init file

--- app/instance/config.py
+++ app/instance/config.py
@@ -1,6 +1,2 @@
-class Config:
-    '''
-    General configuration parent class
-    '''
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-# NEWS_API_KEY='61ba7dd4ee0a49a193c3244f342be313'
+MOVIE_API_KEY='61ba7dd4ee0a49a193c3244f342be313'
+SECRET_KEY ='Flask WTF Secret key'
--- app/news_test.py
+++ app/news_test.py
@@ -7,7 +7,7 @@ class NewsTest(unittest.TestCase):
 
     def setUp(self):
         
-        self.new_news = News(1234,'Python Must Be Crazy','A thrilling new Python news','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+        self.new_news = News(1234,'Python Must Be Interesting','A thrilling new Python news','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
