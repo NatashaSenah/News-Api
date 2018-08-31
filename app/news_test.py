b6db486c394f21423b2fd651f888d8d1eb22a87f
NatashaSenah <tashsenah@gmail.com>
2018-08-31 11:09:19 +0300

    add the created news_test.py_

---
+++ app/news_test.py
@@ -0,0 +1,17 @@
+import unittest
+from models import news
+News = news.news
+
+class NewsTest(unittest.TestCase):
+    
+
+    def setUp(self):
+        
+        self.new_news = News(1234,'Python Must Be Crazy','A thrilling new Python news','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+
+if __name__ == '__main__':
+    unittest.main()
