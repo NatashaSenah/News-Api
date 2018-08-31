98eb0867d0b5464c290726fccbf70aaeb562262a
NatashaSenah <tashsenah@gmail.com>
2018-08-31 12:15:54 +0300

    add the created news

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    message = 'NEWS HIGHLIGHT'
+    return render_template('index.html',message = message)
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('news.html',id = news)
