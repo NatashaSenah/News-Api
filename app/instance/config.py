3525979b7667f9d576dbd9f4c42b7e067c31503c
NatashaSenah <tashsenah@gmail.com>
2018-08-31 10:53:34 +0300

    add the created request.py

--- app/instance/config.py
+++ app/instance/config.py
@@ -1,1 +1,6 @@
-NEWS_API_KEY='61ba7dd4ee0a49a193c3244f342be313'
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+# NEWS_API_KEY='61ba7dd4ee0a49a193c3244f342be313'
